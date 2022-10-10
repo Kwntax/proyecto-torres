2c9fba994bdf2b857f73e31891dd61771e3a8325
Agora <devilman2696@outlook.com>
2022-10-10 09:59:29 -0700

    actualizacion

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/test',function(){
+    return "Hola mundo";
+});
+
+//Route::apiResource('/alumnos',StudentListController::class);
+Route::apiResource('alumnos',StudentListController::class);
+
+Route::apiResource('catalogos',CatalogosController::class);
+
+Route::apiResource('grupos',GruposController::class);
